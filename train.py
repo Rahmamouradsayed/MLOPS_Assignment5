18e044c80cd459877d9a0f3ce452615d03b9b9f5
Rahmamouradsayed <s-rahma.sayed@zewailcity.edu.eg>
2026-03-25 01:15:47 +0200

    Fix train.py - remove log_model

--- train.py
+++ train.py
@@ -1,6 +1,5 @@
 ﻿import os
 import mlflow
-import mlflow.sklearn
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
@@ -24,11 +23,13 @@ with mlflow.start_run() as run:
     clf.fit(X_train, y_train)
     y_pred = clf.predict(X_test)
     accuracy = accuracy_score(y_test, y_pred)
+
     mlflow.log_param("n_estimators", n_estimators)
     mlflow.log_param("max_depth", max_depth)
     mlflow.log_metric("accuracy", accuracy)
-    mlflow.sklearn.log_model(clf, "model")
+
     print(f"Run ID: {run.info.run_id}")
     print(f"Accuracy: {accuracy:.4f}")
+
     with open("model_info.txt", "w") as f:
         f.write(run.info.run_id)
